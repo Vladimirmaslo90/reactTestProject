a7f03ce8662dab9565a1c5b7fee2f05150142c52
Unknown <Vladimirmaslo90@gmail.com>
2018-11-05 16:45:05 +0200

    Changed the way we initialize counter component
    Changed the way we initialize counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.counter.value
+    // tags: ["tag1", "tag2", "tag3"]
   };
 
   //   constructor() {
@@ -43,7 +43,7 @@ class Counter extends Component {
         {/* {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()} */}
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
