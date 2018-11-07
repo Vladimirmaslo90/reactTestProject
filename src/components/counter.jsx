4fe4b2c17d42cb6db4efb2af064fe021af59d116
Volodymyr Maslo <Vladimirmaslo90@gmail.com>
2018-11-07 22:02:02 +0200

    Changed counter component structure
    Added '-' button and implemented method for descreasing values

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -31,23 +31,63 @@ class Counter extends Component {
 
   render() {
     return (
-      <div>
-        {/* <span style {{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()} </span> */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()} </span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="bth btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()} */}
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
+      // <div>
+      //   {/* <span style {{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()} </span> */}
+      //   <span className={this.getBadgeClasses()}>{this.formatCount()} </span>
+      //   <button
+      //     onClick={() => this.props.onIncrement(this.props.counter)}
+      //     className="bth btn-secondary btn-sm"
+      //   >
+      //     +
+      //   </button>
+      //   {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+      //   {this.renderTags()} */}
+      //   <button
+      //     onClick={() => this.props.onDecrement(this.props.counter)}
+      //     className="bth btn-secondary btn-sm"
+      //   >
+      //     -
+      //   </button>
+      //   <button
+      //     onClick={() => this.props.onDelete(this.props.counter.id)}
+      //     className="btn btn-danger btn-sm m-2"
+      //   >
+      //     Delete
+      //   </button>
+      // </div>
+
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col-md-auto">
+            <span className={this.getBadgeClasses()}>
+              {this.formatCount()}{" "}
+            </span>
+          </div>
+          <div class="col col-sm-2">
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="bth btn-secondary btn-sm"
+            >
+              +
+            </button>
+          </div>
+          <div class="col col-sm-2">
+            <button
+              onClick={() => this.props.onDecrement(this.props.counter)}
+              className="bth btn-secondary btn-sm"
+            >
+              -
+            </button>
+          </div>
+          <div class="col col-sm-2">
+            <button
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-sm"
+            >
+              X
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
